40ea2112ab681b5fbcade1887764d68dcb3c26df
Devon Bull <dbull@live.com>
2017-03-14 19:23:29 -0600

    create ball and ball tests, tracks paddle

---
+++ test/Ball-test.js
@@ -0,0 +1,38 @@
+var assert = require('chai').assert;
+var Ball = require('../lib/Ball');
+
+describe ('Ball', function() {
+  it ('should be function', function () {
+    assert.isFunction(Ball);
+  })
+
+  it ('should instantiate a new object', function () {
+    var ball = new Ball ({});
+    assert.isObject(ball);
+  })
+
+  it ('should take height', function () {
+    var ball = new Ball({height: 10});
+    assert.equal(ball.height, 10);
+  })
+
+  it ('should take width', function () {
+    var ball = new Ball({height: 10, width: 10});
+    assert.equal(ball.width, 10);
+  })
+
+  it('should take x axis', function () {
+    var ball = new Ball({x: 135, height: 10, width: 10});
+    assert.equal(ball.x, 135);
+  })
+
+  it('should have set y axis', function () {
+    var ball = new Ball({x: 135, y:60, height: 10, width: 50});
+    assert.equal(ball.y, 60);
+  })
+
+  it('should take a color', function () {
+    var ball = new Ball({x:135, y:60, height: 10, width: 50, color: 'red'})
+    assert.equal(ball.color, 'red')
+  })
+})
